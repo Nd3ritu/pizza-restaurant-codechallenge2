e105e900cc51b6f6207acbfbbe72044e83965a06
Matthew <+>
2024-01-29 15:32:00 +0300

    added validations to prices

--- server/app.py
+++ server/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request,make_response
 from flask_migrate import Migrate
-
+from flask_restful import Api, Resource
 from flask_cors import CORS
-from sqlalchemy.exc import IntegrityError
 
-from models import db, Restaurant
+from models import db, Restaurant ,Pizza, RestaurantPizza
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///respizza.db'
@@ -13,8 +12,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 CORS(app)
 migrate = Migrate(app,db)
 db.init_app(app)
+api = Api(app)
 
-@app.route('/')
-def index():
-    response_body = {"Message":"This restaurant-pizza API"}
-    return make_response(jsonify(response_body), 200)
+class Index(Resource):
+    def get(self):
+        response_body = {
+            "index":"Pizza-Restaurant API"
+        }
+
+        return make_response(jsonify(response_body),200)
+api.add_resource(Index, '/')
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,13 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy import CheckConstraint
-
+from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
-class Restaurant(db.Model, SerializerMixin):
+class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
-    serialize_rules = ('-restaurantpizzas.restaurant')
     
     id = db.Column(db.Integer, primary_key= True)
     name = db.Column(db.String)
@@ -16,11 +13,21 @@ class Restaurant(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     restaurantpizzas = db.relationship('RestaurantPizza', backref ='restaurant')
+    
+    @validates('name')
+    def validate_description(self,key,name):
+        if not name:
+            raise ValueError("Name must be present")
+        if len(name) < 4:
+            raise ValueError("Name must be at least 4 characters long")
+        
+    def __repr__(self) :
+        return f'<Restaurant {self.name}>'
 
-class Pizza(db.Model, SerializerMixin):
+class Pizza(db.Model):
     __tablename__ = 'pizzas'
 
-    serialize_rules = ('-restaurantpizzas.pizza',)
+    
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String)
@@ -29,10 +36,13 @@ class Pizza(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     restaurantpizzas = db.relationship('RestaurantPizza', backref='pizza')
 
-class RestaurantPizza(db.Model, SerializerMixin):
+    def __repr__(self):
+        return f'<Pizza{self.name}>'
+
+class RestaurantPizza(db.Model):
     __tablename__ = 'restaurantpizzas'
 
-    serialize_rules = ('-restaurant.restaurantpizzas', '-pizza.resturantpizza')
+    
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer)
@@ -40,3 +50,14 @@ class RestaurantPizza(db.Model, SerializerMixin):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
     pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+
+    @validates('prices')
+    def validate_price(self,key, price):
+        if not (1 <= int(price) <= 30):
+            raise ValueError("Price must be betweem 1 and 30.")
+        return price
+    
+    def __repr__(self):
+        return f'<RestaurantPizzas{self.price}>'
+
+    
